ea501a0b00a08789cf4ef868e0b15fdf85e53f9f
ItsSuperPlayz <91819449+ItsSuperPlayz@users.noreply.github.com>
2024-09-21 23:41:43 +0700

    Add files via upload

--- wsu.bat
+++ wsu.bat
@@ -10,14 +10,16 @@ net stop beep>nul 2>nul
 :selector
 cls&echo Windows Scripts Utility (build 092024)&echo.&echo    [1] Chris Titus Tech^'s Windows Utility&echo    [2] Microsoft Activation Scripts&echo    [3] Windows10Debloater&echo    [4] Windows11Debloat&echo    [!esc![92m0!esc![0m] Quit&echo.&echo !esc![93mChoose an option using your keyboard...!esc![0m&choice /c 12340 /n /cs&cls&goto optn!errorlevel!
 :optn1
-echo !esc![30m!esc![43mThis script can be found at https://github.com/ChrisTitusTech/winutil!esc![0m&call :checkconnection&echo ^> Executing script...&start /wait powershell -Command "Start-Process -Verb RunAs powershell '-NoExit -ExecutionPolicy Bypass -Command irm https://christitus.com/win|iex'" 2>nul&echo ^> The script has exited.&echo.&echo !esc![93mPress any key to go back...!esc![0m&pause>nul&goto selector
+set link=https://github.com/ChrisTitusTech/winutil&set command=start /wait powershell -Command "Start-Process -Verb RunAs powershell '-NoExit -ExecutionPolicy Bypass -Command irm https://christitus.com/win|iex'"&goto exec
 :optn2
-echo !esc![30m!esc![43mThis script can be found at https://github.com/massgravel/Microsoft-Activation-Scripts!esc![0m&call :checkconnection&echo ^> Executing script...&start /wait powershell -command "irm https://massgrave.dev/get|iex" 2>nul&echo ^> The script has exited.&echo.&echo !esc![93mPress any key to go back...!esc![0m&pause>nul&goto selector
+set link=https://github.com/massgravel/Microsoft-Activation-Scripts&set command=start /wait powershell -command "irm https://massgrave.dev/get|iex"&goto exec
 :optn3
-echo !esc![30m!esc![43mThis script can be found at https://github.com/Sycnex/Windows10Debloater!esc![0m&call :checkconnection&echo ^> Executing script...&start /wait powershell -command "iwr -useb https://git.io/debloat|iex" 2>nul&echo ^> The script has exited.&echo.&echo !esc![93mPress any key to go back...!esc![0m&pause>nul&goto selector
+set link=https://github.com/Sycnex/Windows10Debloater&set command=start /wait powershell -command "iwr -useb https://git.io/debloat|iex"&goto exec
 :optn4
-echo !esc![30m!esc![43mThis script can be found at https://github.com/Raphire/Win11Debloat!esc![0m&call :checkconnection&echo ^> Executing script...&start /wait powershell -command "irm https://raw.githubusercontent.com/Raphire/Win11Debloat/master/Get.ps1|iex" 2>nul&echo ^> The script has exited.&echo.&echo !esc![93mPress any key to go back...!esc![0m&pause>nul&goto selector
+set link=https://github.com/Raphire/Win11Debloat&set command=start /wait powershell -command "irm https://raw.githubusercontent.com/Raphire/Win11Debloat/master/Get.ps1|iex"&goto exec
 :optn5
 exit
-:checkconnection
-echo.&echo ^> Checking internet connection...&ping google.com -n 1 >nul&if !errorlevel! neq 0 (echo !esc![91m^> It looks like you don^'t have an internet connection. Please connect to the internet and try again.!esc![0m&echo.&echo.&echo !esc![93mPress any key to go back...!esc![0m&pause>nul&goto selector) else echo ^> !esc![92mConnection check passed^^!!esc![0m
+:exec
+echo !esc![30m!esc![43mThis script can be found at: %link%!esc![0m&call :connection&echo ^> Executing script...&%command% >nul 2>nul&echo ^> The script has exited.&echo.&echo !esc![93mPress any key to go back...!esc![0m&pause>nul&goto selector
+:connection
+echo.&echo ^> Checking internet connection...&ping google.com -n 1 >nul&if !errorlevel! neq 0 (echo ^> !esc![91mIt looks like you don^'t have an internet connection. Please connect to the internet and try again.!esc![0m&echo.&echo !esc![93mPress any key to go back...!esc![0m&pause>nul&goto selector) else echo ^> !esc![92mConnection check passed^^!!esc![0m
